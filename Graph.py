1400331a27a4bb1b0aa6442c2f3eff215d3848af
Bhanuteja <bhanutejach9@gmail.com>
2022-07-23 23:37:19 +0530

    initial commit - moving project

---
+++ Graph.py
@@ -0,0 +1,18 @@
+import Queue
+
+class Graph():
+    def __init__(self):
+        self.a = dict()
+
+    def __init__(self, a):
+        self.a = a
+    
+    eq = Queue()
+
+    #a - adjacency List
+    #v - source vertex
+    def bfs(self, a, v):
+        visited = dict()
+        
+        
+    
