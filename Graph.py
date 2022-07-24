b77eebc7bfd3e5bc03441f2e65fd11e928474454
Bhanuteja <bhanutejach9@gmail.com>
2022-07-24 13:36:58 +0530

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -1,18 +1,32 @@
 import Queue
 
 class Graph():
-    def __init__(self):
-        self.a = dict()
-
-    def __init__(self, a):
-        self.a = a
+    def __init__(self, adjList):
+        self.adjList = adjList
     
-    eq = Queue()
+    explorationQueue = Queue()
 
-    #a - adjacency List
-    #v - source vertex
-    def bfs(self, a, v):
-        visited = dict()
+    def bfs(self, adjList, sourceVertex):
+        (visited, parent, distance) = (dict(), dict(), dict())
+        for u in adjList.keys():
+            visited[u] = False
+            parent[u] = -1
+            distance[u] = -1
         
+        explorationQueue = Queue()
+
+        visited[sourceVertex] = True
+        distance[sourceVertex] = 0
+
+        explorationQueue.add(sourceVertex)
+
+        while(not explorationQueue.isempty()):
+            u = explorationQueue.delete()             
+            for v in adjList[u]:
+                if(not visited[v]):                   
+                    visited[v] = True
+                    parent[v] = u
+                    distance[v] = distance[u] + 1
         
-    
+        return(parent, distance)
+               
